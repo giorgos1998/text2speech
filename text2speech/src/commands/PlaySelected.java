fd622ce4afeea7ae72b5a7ec525f7d423eb84168
giorgos1998 <george.papadatos98@gmail.com>
2020-04-14 13:59:38 +0300

    Added 4 commands, Added Document functionality

---
+++ text2speech/src/commands/PlaySelected.java
@@ -0,0 +1,24 @@
+package commands;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import gui.FreeTTSWindow;
+import model.Document;
+
+public class PlaySelected implements ActionListener{
+	
+	private FreeTTSWindow frame;
+	private Document doc;
+	
+	public PlaySelected(FreeTTSWindow frame, Document doc) {
+		this.frame = frame;
+		this.doc = doc;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String text = frame.getTextArea().getSelectedText();
+		doc.playSelected(text);
+	}
+}
