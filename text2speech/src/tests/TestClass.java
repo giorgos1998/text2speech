97078f527ab79c7385d6d7fcb53f4de7edddf092
jrizxos <jrizos98@gmail.com>
2020-05-10 15:49:54 +0300

    added basic unit test classes

---
+++ text2speech/src/tests/TestClass.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Launcher;
+
+public class TestClass {
+
+	@Test
+	public void testMain() {
+		Launcher.main(null);	
+	}
+
+}
