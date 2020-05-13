4610df4bc88b7d7036a4ef98454bd23afe331d22
jrizxos <jrizos98@gmail.com>
2020-05-13 18:21:10 +0300

    Added some tests (working)

--- text2speech/src/tests/TestClass.java
+++ text2speech/src/tests/TestClass.java
@@ -2,8 +2,9 @@ package tests;
 
 import static org.junit.Assert.*;
 import java.awt.event.ActionListener;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import org.junit.Test;
 
@@ -15,29 +16,48 @@ public class TestClass {
 
 	@Test
 	public void testMain() {
-		//Launcher testLauncher = new Launcher();
 		ActionListener testCommand;
 		Launcher.main(null);
 		FreeTTSWindow frame = Launcher.frame;
 		CommandFactory testCommandFactory =  new CommandFactory();
 		
+		
+		//test making new file
 		testCommand = testCommandFactory.makeCommand("NewFileCommand", frame );
 		testCommand.actionPerformed(null);
 		assertEquals("Text should be empty when a new file is made.", "", frame.getTextArea().getText());
 		
-		
+		//test saving in new file
 		frame.getTextArea().setText("Hello World!");
+		File testFile = new File("testSaveFile.txt");
+		frame.getFileChooser().setSelectedFile(testFile);
 		testCommand = testCommandFactory.makeCommand("SaveFileCommand", frame );
 		testCommand.actionPerformed(null);
-		String fileName = frame.getFileChooser().getSelectedFile().getAbsolutePath();
 		try {
-			FileReader fr = new FileReader(fileName);
+			Scanner fileScanner = new Scanner(testFile);
 			assertEquals("Text in file should be the same as text area.", 
-					"Hello World!", null);
+					"Hello World!", fileScanner.nextLine());
+			fileScanner.close();
 		}
-		catch(IOException e) {
+		catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		//test opening an existing file
+		//you have to select the correct file -.-
+		//(git repo-project folder)...\text2speech\text2speech\testSaveFile.txt
+		testCommand = testCommandFactory.makeCommand("OpenFileCommand", frame );
+		testCommand.actionPerformed(null);
+		try {
+			Scanner fileScanner = new Scanner(testFile);
+			assertEquals("Text in text area should be the same as in file .", 
+					"Hello World!", frame.getTextArea().getText());
+			fileScanner.close();
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 
