4ed98f466a4f4f0210b8cbc5f9751d5e839ab410
jrizxos <jrizos98@gmail.com>
2020-05-18 13:19:50 +0300

    Added tests for US1 to US12

--- text2speech/src/tests/TestClass.java
+++ text2speech/src/tests/TestClass.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import org.junit.Test;
 
 import main.Launcher;
+import model.Document;
 import commands.CommandFactory;
 import gui.FreeTTSWindow;
 
@@ -17,10 +18,11 @@ public class TestClass {
 	@Test
 	public void testMain() {
 		ActionListener testCommand;
+		File testFile = new File("testSaveFile.txt");
+		CommandFactory testCommandFactory =  new CommandFactory();
+		Document doc;
 		Launcher.main(null);
 		FreeTTSWindow frame = Launcher.frame;
-		CommandFactory testCommandFactory =  new CommandFactory();
-		
 		
 		//test making new file (US1)
 		testCommand = testCommandFactory.makeCommand("NewFileCommand", frame );
@@ -29,7 +31,6 @@ public class TestClass {
 		
 		//test saving in new file (US3)
 		frame.getTextArea().setText("Hello World!");
-		File testFile = new File("testSaveFile.txt");
 		frame.getFileChooser().setSelectedFile(testFile);
 		testCommand = testCommandFactory.makeCommand("SaveFileCommand", frame );
 		testCommand.actionPerformed(null);
@@ -50,7 +51,7 @@ public class TestClass {
 		testCommand.actionPerformed(null);
 		try {
 			Scanner fileScanner = new Scanner(testFile);
-			assertEquals("Text in text area should be the same as in file .", 
+			assertEquals("Text in text area should be the same as in file.", 
 					"Hello World!", frame.getTextArea().getText());
 			fileScanner.close();
 		}
@@ -58,25 +59,70 @@ public class TestClass {
 			e.printStackTrace();
 		}
 		
-		//test "playing sound" with play all command
+		//test "playing sound" with PlayAll command (US5-6)
+		/* UFINISHED, needs reference to doc
 		frame.getTextArea().setText("Hello World!");
-		frame.setSpeechLibrary("FAKETTS");
-		//testCommand = testCommandFactory.makeCommand("TuneAudioCommand", frame );
-		//testCommand.actionPerformed(null);
+		testFile = new File("FakeTTSApiOut.txt");
+		doc.saveSettings(50,21,11,"ROT13","FAKETTS");
 		testCommand = testCommandFactory.makeCommand("PlayAllCommand", frame );
 		testCommand.actionPerformed(null);
-		testFile = new File("FakeTTSApiOut.txt");
 		try {
 			Scanner fileScanner = new Scanner(testFile);
-			assertEquals("Text in text area should be the same as in file .", 
-					fileScanner.nextLine(), frame.getTextArea().getText());
+			assertEquals("Text in text area should be the same as in file.", 
+					fileScanner.nextLine(), "Hello World!");
 			fileScanner.close();
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
+		}*/
+		
+		//test "playing sound" in reverse with PlayAllReverse command (US7-8)
+		/* UFINISHED, needs reference to doc
+		frame.getTextArea().setText("Hello World!");
+		testFile = new File("FakeTTSApiOut.txt");
+		//doc.saveSettings(50,21,11,"ROT13","FAKETTS");
+		testCommand = testCommandFactory.makeCommand("PlayAllReverse", frame );
+		testCommand.actionPerformed(null);
+		try {
+			Scanner fileScanner = new Scanner(testFile);
+			assertEquals("Text in file should be the reverse of text area.", 
+					fileScanner.nextLine(), "World! Hello");
+			fileScanner.close();
 		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}*/
+		
+		//test "playing sound" encoded with PlayAllEncoded command (US9-10)
+		/* UFINISHED, needs reference to doc
+		frame.getTextArea().setText("Hello World!");
+		testFile = new File("FakeTTSApiOut.txt");
+		//doc.saveSettings(50,21,11,"ROT13","FAKETTS");
+		testCommand = testCommandFactory.makeCommand("PlayAllEncoded", frame );
+		testCommand.actionPerformed(null);
+		try {
+			Scanner fileScanner = new Scanner(testFile);
+			assertEquals("Text in file should be the encoded of text area.", 
+					fileScanner.nextLine(), "Uryyb Jbeyq!");
+			fileScanner.close();
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}*/
+		
+		//test settings (US11-12)
+		frame.setVolumeValue(100);
+		assertEquals("Volume in frame should be the same as set.",frame.getVolumeValue(),100);
+		frame.setSpeedValue(100);
+		assertEquals("Volume in frame should be the same as set.",frame.getSpeedValue(),100);
+		frame.setPitchValue(100);
+		assertEquals("Volume in frame should be the same as set.",frame.getPitchValue(),100);
+		frame.setEncodingStrategy("ATBASH");
+		assertEquals("Volume in frame should be the same as set.",frame.getEncodingStrategy(),"ATBASH");
+		frame.setSpeechLibrary("FREETTS");
+		assertEquals("Volume in frame should be the same as set.",frame.getSpeechLibrary(),"FREETTS");
 		
 		
 	}
-
+	
 }
